0dc0c8f51aa2f235d55da7242299c33edb8769af
gsoumya <gsoumya515@gmail.com>
2019-06-10 14:49:00 +0530

    changed callback into promises

--- index.js
+++ index.js
@@ -6,36 +6,43 @@ const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
 MongoClient.connect(url, (err, client) => {
-
-    assert.equal(err,null);
-
+    assert.equal(err, null);
     console.log('Connected correctly to server');
-
     const db = client.db(dbname);
     const collection = db.collection("dishes");
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
-        "dishes", (result) => {
-            console.log("Insert Document:\n", result.ops);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test" },
+        "dishes").then((result) => {
+            console.log(result, 're');
 
-            dboper.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);
+            return dboper.findDocuments(db, "dishes")
+        }).then((data) => {
+            console.log(data, 'DDIIIS')
 
-                dboper.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes",
-                    (result) => {
-                        console.log("Updated Document:\n", result.result);
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                { description: "Updated Test" }, "dishes")
 
-                        dboper.findDocuments(db, "dishes", (docs) => {
-                            console.log("Found Updated Documents:\n", docs);
-                            
-                            // db.dropCollection("dishes", (result) => {
-                            //     console.log("Dropped Collection: ", result);
+        })
+        .then((updataeddata) => {
+            console.log(updataeddata, 'updataeddata')
 
-                            //     client.close();
-                            // });
-                        });
-                    });
-            });
-    });
-   
-    });
+            return dboper.findDocuments(db, "dishes")
+
+        }).then((data) => {
+
+            console.log(data, 'Dishes after update')
+            // db.dropCollection("dishes", (result) => {
+            //     console.log("Dropped Collection: ", result);
+
+            //     client.close();
+            // });
+
+
+
+
+
+        }).catch((error) => {
+            console.log(error, " error dishes errrorrrr");
+        })
+
+
+});
--- operations.js
+++ operations.js
@@ -1,6 +1,9 @@
 const assert = require('assert');
 
-exports.insertDocument = (db, document, collection, callback) => {
+
+// callback type
+/**
+ * exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
     coll.insert(document, (err, result) => {
         assert.equal(err, null);
@@ -16,21 +19,24 @@ exports.findDocuments = (db, collection, callback) => {
         callback(docs);        
     });
 };
+**/
 
-exports.removeDocument = (db, document, collection, callback) => {
+// promise type
+exports.insertDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Removed the document ", document);
-        callback(result);        
-    });
+    return coll.insert(document)
+};
+exports.findDocuments = (db, collection) => {
+    const coll = db.collection(collection);
+    return coll.find({}).toArray()
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("Updated the document with ", update);
-        callback(result);        
-    });
+  return  coll.deleteOne(document)
+};
+
+exports.updateDocument = (db, document, update, collection) => {
+    const coll = db.collection(collection);
+    coll.updateOne(document, { $set: update }, null)
 };
